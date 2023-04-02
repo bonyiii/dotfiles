c9a48922c71f03ca360e5941c8d67392e91bc6b2
boni <boni@twine.hu>
2023-04-02 10:43:15 +0200

    Update whitespace mode

--- home/.emacs.d/packages/init-whitespace.el
+++ home/.emacs.d/packages/init-whitespace.el
@@ -1,18 +1,37 @@
-(use-package whitespace
-  :config
-  ;;http://ergoemacs.org/emacs/whitespace-mode.html
-  ;; The proper space sign was taken from here.
-  ;; To get documentation type: C-h v whitespace-display-mappings
-  ;; https://www.codetable.net/unicodecharacters
-  (setq whitespace-display-mappings
-	'((space-mark   ?\    [?\x20]     [?.])	; space
-          (space-mark   ?\xA0 [?\xA4]     [?_])	; hard space
-          (newline-mark 10   [?¬ ?\n] [?¬ ?\n])	; end-of-line
-          )))
+;; (use-package whitespace
+;;   :config
+;;   ;;http://ergoemacs.org/emacs/whitespace-mode.html
+;;   ;; The proper space sign was taken from here.
+;;   ;; To get documentation type: C-h v whitespace-display-mappings
+;;   ;; https://www.codetable.net/unicodecharacters
+;;   (setq whitespace-display-mappings
+;; 	'((space-mark   ?\    [?\x20]     [?.])	; space
+;;           (space-mark   ?\xA0 [?\xA4]     [?_])	; hard space
+;;           (newline-mark 10   [?¬ ?\n] [?¬ ?\n])	; end-of-line
+;;           )))
 
-;; Delete trailing whitespace on save
-(defun nuke_traling ()
-  (add-hook 'before-save-hook #'delete-trailing-whitespace nil t))
-;;(add-hook 'focus-out-hook #'nuke_traling)
+;; ;; Delete trailing whitespace on save
+;; (defun nuke_traling ()
+;;   (add-hook 'before-save-hook #'delete-trailing-whitespace nil t))
+;; ;;(add-hook 'focus-out-hook #'nuke_traling)
+
+
+
+;;; package --- Whitespace setup
+;;; Commentary:
+;;; Code:
+
+(require 'whitespace)
+(setq whitespace-style '(face tabs lines-tail trailing))
+(setq whitespace-action '(cleanup))
+(setq whitespace-display-mappings
+      '((space-mark   ?\    [?\x20]     [?.]) ; space
+        (space-mark   ?\xA0 [?\xA4]     [?_])     ; hard space
+        (newline-mark 10   [?¬ ?\n] [?¬ ?\n])     ; end-of-line
+        ))
+
+(global-whitespace-mode t)
+
 
 (provide 'init-whitespace)
+
